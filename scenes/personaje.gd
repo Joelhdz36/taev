0c96a743cf1b07b21e5e07711de5dc502a46cecb
Joelhdz36 <joel-lv@hotmail.com>
2025-11-16 21:44:49 -0600

    basico implementado
    funciones basicas del menu de pausa agregadas.

--- scenes/personaje.gd
+++ scenes/personaje.gd
@@ -3,21 +3,31 @@ extends CharacterBody2D
 
 @export var SPEED = 300.0
 
+#GUIDE input
+var player_context:GUIDEMappingContext = preload("uid://bcnkmasrse8hk")
+var movement_action:GUIDEAction = preload("uid://cj1j03264ex7p")
+var run_action:GUIDEAction = preload("uid://iydbahyoh7tg")
 
 const JUMP_VELOCITY = -400.0
 
 
+func _ready() -> void:
+	GUIDE.enable_mapping_context(player_context)
+
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var speed:float
+	speed = SPEED
+	## Add the gravity.
+	#if not is_on_floor():
+		#velocity += get_gravity() * delta
+	if run_action.is_triggered():
+		speed = SPEED * 2.5
+	## Get the input direction and handle the movement/deceleration.
+	## As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := movement_action._value_axis_2d
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction.x * speed * delta
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.x = move_toward(velocity.x, 0, speed)
+#
 	move_and_slide()
