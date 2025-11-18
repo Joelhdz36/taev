51f89915ab3920d8e1411def7c7b4681df5cf29c
Joelhdz36 <joel-lv@hotmail.com>
2025-11-17 22:36:03 -0600

    items display

--- joel/scenes/UI/inventario.gd
+++ joel/scenes/UI/inventario.gd
@@ -1,13 +1,16 @@
 extends CanvasLayer
 
-@onready var h_box_container: HBoxContainer = %HBoxContainer
+@onready var image_visualizer: PanelContainer = %ImageVisualizer
+
 
 @export var icons:Array[ColorRect] = []
 
 var inventory_item_scene:PackedScene = preload("uid://c0cayd5fr4mjn")
-
+var inventory_index:int = 0
 #GUIDE
 var inventory_action:GUIDEAction = preload("uid://cnp3rs8pb2o7a")
+
+
 
 func _process(_delta: float) -> void:
 	if inventory_action.is_triggered():
@@ -15,11 +18,34 @@ func _process(_delta: float) -> void:
 		queue_free()
 
 func create_inventory():
-	for item in InventoryManager.objects_in_inventory:
-		create_item(item)
-
+	if InventoryManager.objects_in_inventory.size() > 0:
+		create_item(InventoryManager.objects_in_inventory[inventory_index])
+	else: return
 
 func create_item(item_to_create:Resource):
+	clear_visualizer()
 	var new_item = inventory_item_scene.instantiate()
-	h_box_container.add_child(new_item)
+	image_visualizer.add_child(new_item)
 	new_item.display_info(item_to_create)
+
+func clear_visualizer():
+	if image_visualizer.get_child_count() > 0:
+		for child in image_visualizer.get_children():
+			child.queue_free()
+
+
+
+func _on_left_btn_pressed() -> void:
+	if inventory_index > 0:
+		inventory_index -= 1
+	else:
+		inventory_index = InventoryManager.objects_in_inventory.size() -1
+	create_item(InventoryManager.objects_in_inventory[inventory_index])
+
+
+func _on_right_btn_pressed() -> void:
+	if inventory_index < InventoryManager.objects_in_inventory.size() -1:
+		inventory_index += 1
+	else:
+		inventory_index = 0
+	create_item(InventoryManager.objects_in_inventory[inventory_index])
--- joel/scenes/inventory/inventory_item.gd
+++ joel/scenes/inventory/inventory_item.gd
@@ -3,3 +3,14 @@ extends Control
 
 func display_info(item:Resource):
 	self.texture = item.inventory_icon
+	self_modulate = random_color()
+
+
+func random_color() -> Color:
+	var new_color:Color
+	new_color.r = randf()
+	new_color.g = randf()
+	new_color.b = randf()
+	new_color.a = 1.0
+	
+	return new_color
