9d0811865fbb2b5ed08ffb71ed9c705836deb1c1
Joelhdz36 <joel-lv@hotmail.com>
2025-11-19 21:24:30 -0600

    mecanicas basicas
    El personaje puede recibir y causar daño
    Los enemigos pueden causar daño y morir

--- ch_enemy.gd
+++ ch_enemy.gd
@@ -9,16 +9,18 @@ func _ready() -> void:
 	set_initial_values()
 	print(_health,_damage)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 func set_initial_values():
 	_health = enemy_resource.health
 	_damage = enemy_resource.damage
 
+func take_damage(damage:float):
+	_health -= damage
+	die()
 
-
+func die():
+	if _health <= 0:
+		queue_free()
 
 func _on_damage_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
