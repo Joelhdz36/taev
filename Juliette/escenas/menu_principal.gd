7c935f202bd09d7463d916b4910fd3ca90d4dbba
Juliette0232 <avina0232@gmail.com>
2025-12-03 12:28:10 -0600

    Menu principal-Final, esperando revision

--- Juliette/escenas/menu_principal.gd
+++ Juliette/escenas/menu_principal.gd
@@ -3,3 +3,11 @@ extends Control
 
 func _on_iniciar_pressed() -> void:
 	SceneManager.cambio_escena("uid://dsiu560phtwcg")
+
+
+func _on_ajustes_pressed() -> void:
+	SceneManager.cambio_escena("uid://c4mqr7824uavn")
+
+
+func _on_salir_pressed() -> void:
+	get_tree().quit()
